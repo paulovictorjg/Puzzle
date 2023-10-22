52f8320b1ca58209965fe82481c50891a81fc2a3
Paulo Victor <89660799+paulovictorjg@users.noreply.github.com>
2023-10-22 18:09:01 -0300

    jogo com imagem nao esta sorteando sozinho

--- app.js
+++ app.js
@@ -1,119 +1,129 @@
-//pega o botão
 let newGameButton = document.getElementById('game');
-
-//flag para determinar a ordem de pressionar o botão
 let newButtonClicked = true;
-
-//evento quando o botão é clicado
 newGameButton.onclick = start;
-
-//contador de passos
 let moveCounter = 0;
 let gameTime = 0;
 
-//acionado quando o botão de novo jogo é clicado
+// URL da imagem
+const image = 'url("./img/4x4img.jpg")';
+
+// Acionado quando o botão de novo jogo é clicado
 function start() {
   let dif = document.querySelector('[name="game-difficulty"]:checked').value;
   if (newButtonClicked) {
     startGame(dif);
     field.style.display = 'block';
     startResults();
-    newButtonClicked = false; //o valor da flag muda
+    newButtonClicked = false; // O valor da flag muda
     newGameButton.innerHTML = 'Restart';
     difficulty.style.display = 'none';
     result.style.display = 'block';
   } else {
-    window.location.reload(); //recarregue a página
+    window.location.reload(); // Recarregue a página
   }
 }
-
 function startGame(dif) {
-  //procurando o campo
+  // Procurando o campo
   const field = document.querySelector('.field');
-
-  //tamanho da célula
+  // Tamanho da célula
   const cellSize = 100;
-
-  //adiciona as informações sobre as células em um array
+  // Adiciona as informações sobre as células em um array
   const cells = [];
-
-  //cria e ordena um array com números
+  // Cria e ordena um array com números
   const numbers = [...Array(dif * dif - 1).keys()].sort(
     () => Math.random() - 0.5
   );
+  const fullImage = document.getElementById('full-image');
+  if (dif == 4 && document.getElementById('game-difficulty-img').checked) {
+    fullImage.src = './img/4x4img.jpg';
+    fullImage.style.display = 'block';
+  } else {
+    fullImage.style.display = 'none';
+  }
 
   for (let i = 0; i <= dif * dif - 2; i++) {
-    //cria uma tag
+    // Cria uma tag
     const cell = document.createElement('div');
     const value = numbers[i] + 1;
     cell.className = 'cell';
-    //escreve o valor na célula
-    cell.innerHTML = value;
-    //posição na linha
+    // Verifique se a opção selecionada é "4x4 (imagem)"
+    if (dif == 4 && document.getElementById('game-difficulty-img').checked) {
+      // Calcule a posição da imagem de fundo
+      const x = (i % dif) * cellSize;
+      const y = Math.floor(i / dif) * cellSize;
+      cell.style.backgroundImage = image;
+      cell.style.backgroundPositionX = `-${x}px`;
+      cell.style.backgroundPositionY = `-${y}px`;
+    } else {
+      // Escreve o valor na célula
+      cell.innerHTML = value;
+    }
+    // Posição na linha
     const left = i % dif;
-    //posição na coluna
+    // Posição na coluna
     const top = (i - left) / dif;
-    //escreve a célula no array
+    // Escreve a célula no array
     cells.push({
       value: value,
       left: left,
       top: top,
       element: cell,
     });
-    //muda as coordenadas
+    // Muda as coordenadas
     cell.style.left = `${left * cellSize}px`;
     cell.style.top = `${top * cellSize}px`;
-
-    //adiciona uma célula ao campo field
+    // Adiciona uma célula ao campo field
     field.append(cell);
-
-    //quando o manipulador de eventos é acionado, a função é acionada
+    // Quando o manipulador de eventos é acionado, a função é acionada
     cell.addEventListener('click', () => {
       move(i);
     });
   }
 
-  //coordenadas de uma célula vazia
+  // Coordenadas de uma célula vazia
   const empty = {
     value: dif * dif,
     top: dif - 1,
     left: dif - 1,
   };
-  //adiciona uma célula vazia ao array
+
+  // Adiciona uma célula vazia ao array
   cells.push(empty);
 
   function move(index) {
-    //pega a célula
+    // Pega a célula
     const cell = cells[index];
 
-    //pega a diferença na coordenada
+    // Pega a diferença na coordenada
     const leftDiff = Math.abs(empty.left - cell.left);
     const topDiff = Math.abs(empty.top - cell.top);
 
-    //se a diferença for maior que um, então a célula não é adjacente
+    // Se a diferença for maior que um, então a célula não é adjacente
     if (leftDiff + topDiff > 1) {
       return;
     }
+
     moveCounter++;
-    //mover para a célula anterior
+
+    // Mover para a célula anterior
     cell.element.style.left = `${empty.left * cellSize}px`;
     cell.element.style.top = `${empty.top * cellSize}px`;
 
-    //coordenadas de uma célula vazia
+    // Coordenadas de uma célula vazia
     const emptyLeft = empty.left;
     const emptyTop = empty.top;
 
-    //anota as coordenadas da célula atual
+    // Anota as coordenadas da célula atual
     empty.left = cell.left;
     empty.top = cell.top;
 
-    //passa os valores gravados para a célula
+    // Passa os valores gravados para a célula
     cell.left = emptyLeft;
     cell.top = emptyTop;
 
-    //o método each verifica a condição de cada elemento
+    // O método each verifica a condição de cada elemento
     const isFinished = cells.every((cell) => {
-      //verifica a coordenada correta
+      // Verifica a coordenada correta
       return cell.value === cell.top * dif + cell.left + 1;
     });
 
@@ -129,17 +139,20 @@ function startGame(dif) {
     }
   }
 }
-//resultados
+
+// Resultados
 function startResults() {
   const moveContainer = document.querySelector('.move-text');
   const timeContainer = document.querySelector('.time-text');
   moveContainer.innerHTML = '' + moveCounter;
   timeContainer.innerHTML = '' + gameTime;
-  //atualiza o contêiner de etapas
+
+  // Atualiza o contêiner de etapas
   const movesUpdate = setInterval(() => {
     moveContainer.innerHTML = '' + moveCounter;
   }, 100);
-  //atualiza o contêiner de tempo
+
+  // Atualiza o contêiner de tempo
   const gameInterval = setInterval(() => {
     timeContainer.innerHTML = '' + ++gameTime;
   }, 1000);
